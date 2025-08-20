ad98c4095a7391e936e17450fbd3a05edd05dce5
Pari Malam <apid9755@gmail.com>
2025-08-20 11:02:32 +0800

    released v1.0

--- modules/banners.py
+++ modules/banners.py
@@ -9,21 +9,28 @@ def clear_screen():
 
 # =========================================================================================================== #
 
+
+
+
+
+
+
+                                                                                           
 def banners():
     stdout.write("                                                                                         \n")
-    stdout.write(""+Fore.LIGHTRED_EX +"██████╗  ██████╗  ██████╗              ████████╗ ██████╗  ██████╗ ██╗     ███████╗\n")
-    stdout.write(""+Fore.LIGHTRED_EX +"██╔══██╗██╔═══██╗██╔════╝              ╚══██╔══╝██╔═══██╗██╔═══██╗██║     ██╔════╝\n")
-    stdout.write(""+Fore.LIGHTRED_EX +"██████╔╝██║   ██║██║         █████╗       ██║   ██║   ██║██║   ██║██║     ███████╗\n")
-    stdout.write(""+Fore.LIGHTRED_EX +"██╔══██╗██║   ██║██║         ╚════╝       ██║   ██║   ██║██║   ██║██║     ╚════██║\n")
-    stdout.write(""+Fore.LIGHTRED_EX +"██║  ██║╚██████╔╝╚██████╗                 ██║   ╚██████╔╝╚██████╔╝███████╗███████║\n")
-    stdout.write(""+Fore.LIGHTRED_EX +"╚═╝  ╚═╝ ╚═════╝  ╚═════╝                 ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝╚══════╝\n")
+    stdout.write(""+Fore.LIGHTRED_EX +"██████╗ ██╗   ██╗██╗                   ███╗   ███╗ █████╗  ██████╗██████╗  ██████╗ ███████╗\n")
+    stdout.write(""+Fore.LIGHTRED_EX +"██╔══██╗╚██╗ ██╔╝██║                   ████╗ ████║██╔══██╗██╔════╝██╔══██╗██╔═══██╗██╔════╝\n")
+    stdout.write(""+Fore.LIGHTRED_EX +"██████╔╝ ╚████╔╝ ██║         █████╗    ██╔████╔██║███████║██║     ██████╔╝██║   ██║███████╗\n")
+    stdout.write(""+Fore.LIGHTRED_EX +"██╔══██╗  ╚██╔╝  ██║         ╚════╝    ██║╚██╔╝██║██╔══██║██║     ██╔══██╗██║   ██║╚════██║\n")
+    stdout.write(""+Fore.LIGHTRED_EX +"██║  ██║   ██║   ███████╗              ██║ ╚═╝ ██║██║  ██║╚██████╗██║  ██║╚██████╔╝███████║\n")
+    stdout.write(""+Fore.LIGHTRED_EX +"╚═╝  ╚═╝   ╚═╝   ╚══════╝              ╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝\n")
     stdout.write(""+Fore.YELLOW +"═════════════╦═════════════════════════════════╦══════════════════════════════\n")
     stdout.write(""+Fore.YELLOW   +"╔════════════╩═════════════════════════════════╩═════════════════════════════╗\n")
     stdout.write(""+Fore.YELLOW   +"║ \x1b[38;2;255;20;147m• "+Fore.GREEN+"AUTHOR             "+Fore.RED+"    |"+Fore.LIGHTWHITE_EX+"   PARI MALAM                                    "+Fore.YELLOW+"║\n")
     stdout.write(""+Fore.YELLOW   +"╔════════════════════════════════════════════════════════════════════════════╝\n")
     stdout.write(""+Fore.YELLOW   +"║ \x1b[38;2;255;20;147m• "+Fore.GREEN+"GITHUB             "+Fore.RED+"    |"+Fore.LIGHTWHITE_EX+"   GITHUB.COM/THATNOTEASY                        "+Fore.YELLOW+"║\n")
     stdout.write(""+Fore.YELLOW   +"╚════════════════════════════════════════════════════════════════════════════╝\n") 
-    print(f"{Fore.YELLOW}[ROC-TOOLS] - {Fore.GREEN}RYL AUTOMATION HOTKEYS - {Fore.RED}[V1.0] \n{Fore.RESET}")
+    print(f"{Fore.YELLOW}[RYL-Macros] - {Fore.GREEN}RiskYourLife Macros Hotkeys - {Fore.RED}[V1.0] \n{Fore.RESET}")
 
 # =========================================================================================================== #
 
